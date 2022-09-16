6a94d3ed004e4eddc31490dc6fd613212ec8faeb
wak10 <chiba.r25.taka@gmail.com>
2022-09-16 18:29:39 +0900

    カラム追加

--- db/migrate/20220727130653_create_posts.rb
+++ db/migrate/20220727130653_create_posts.rb
@@ -1,8 +1,8 @@
 class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
-      t.text :message
-      t.timestamps null: false
+      t.text :message, null: false
+      t.timestamps, null: false
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_27_130653) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_16_092159) do
+  create_table "locations", charset: "utf8mb4", force: :cascade do |t|
+    t.string "address", null: false
+    t.float "latitude", null: false
+    t.float "longitude", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "posts", charset: "utf8mb4", force: :cascade do |t|
     t.text "message", null: false
     t.datetime "created_at", null: false
