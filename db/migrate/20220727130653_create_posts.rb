64959e5cedb1dd59c82937129e741cb204bb21c3
wak10 <chiba.r25.taka@gmail.com>
2022-12-02 11:54:13 +0900

    現在地取得

--- db/migrate/20220727130653_create_posts.rb
+++ db/migrate/20220727130653_create_posts.rb
@@ -1,10 +1,10 @@
 class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
-      t.text :message, null: false
-      t.string :address, null: false
-      t.float :latitude,	null: false
-      t.float :longitude,	null: false
+      t.text :message
+      t.string :address
+      t.float :latitude
+      t.float :longitude
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -12,10 +12,10 @@
 
 ActiveRecord::Schema[7.0].define(version: 2022_07_27_130653) do
   create_table "posts", charset: "utf8mb4", force: :cascade do |t|
-    t.text "message", null: false
-    t.string "address", null: false
-    t.float "latitude", null: false
-    t.float "longitude", null: false
+    t.text "message"
+    t.string "address"
+    t.float "latitude"
+    t.float "longitude"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
